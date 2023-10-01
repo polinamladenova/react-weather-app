2147716f6a2cd6dc0f13db26ae24343eeb6298e7
polinamladenova <polinamladenova91@gmail.com>
2023-10-02 01:31:47 +0300

    Moved personal credits back into App component

--- src/App.js
+++ src/App.js
@@ -7,6 +7,30 @@ export default function App() {
     <div className="App">
       <div className="container">
         <Weather />
+        <br />
+        <p className="credits">
+          <a
+            href="https://github.com/polinamladenova/weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open-source code{" "}
+          </a>
+          by
+          <a href="https://www.shecodes.io/" target="_blank" rel="noreferrer">
+            {" "}
+            SheCodes{" "}
+          </a>
+          student
+          <a
+            href="https://www.linkedin.com/in/polina-mladenova-387087a5/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            Polina Mladenova
+          </a>
+        </p>
       </div>
     </div>
   );
--- src/Weather.js
+++ src/Weather.js
@@ -84,30 +84,6 @@ export default function Weather() {
         </button>
         <div></div>
       </span>
-      <br />
-      <p className="credits">
-        <a
-          href="https://github.com/polinamladenova/weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Open-source code{" "}
-        </a>
-        by
-        <a href="https://www.shecodes.io/" target="_blank" rel="noreferrer">
-          {" "}
-          SheCodes{" "}
-        </a>
-        student
-        <a
-          href="https://www.linkedin.com/in/polina-mladenova-387087a5/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          {" "}
-          Polina Mladenova
-        </a>
-      </p>
       <script
         src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
